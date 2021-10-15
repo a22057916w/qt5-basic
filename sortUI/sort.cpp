a85db3f170a7d98ba9427c5c7ae071a94bdbb89a
a22057916w <a22057916w@gmail.com>
2021-10-15 14:35:48 +0800

    new layout and window and app

--- sortUI/sort.cpp
+++ sortUI/sort.cpp
@@ -1,10 +1,42 @@
 #include "main.h"
 
-#include <alogrithm>
+#include <algorithm>
 #include <string>
+#include <vector>
 
+using std::vector;
 using std::string;
 
+/* create delay in qt */
+void delay();
+/* filling array with random number */
+void updateArray();
+/* set the bkg color of button to yellow */
+void highlightButton();
+/* set the button to normal color */
+void unhighlightButton();
+/* setup button */
+void setButton();
+/* run bubble sort and update UI */
+void bubblesort();
+/* run selection sort and update UI */
+void selectionsort();
+
 int main(int argc, char *argv[]) {
-  return 0;
+
+  const int size = 10;
+  vector<int> arr(size, 0);
+
+  // create an instance of QApplication
+  QApplication app(argc, argv);
+  // creating an instance of GUI
+  QWidget window;
+  window.resize(200, 100);
+  window.setWindowTitle("Sort Algorithm");
+
+  // create layout inside window
+  QGridLayout *layout = new QGridLayout(&window);
+
+  window.show();
+  return app.exec();
 }
