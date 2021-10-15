9babd04ff249df724f16e38c45d0eccf9687effd
a22057916w <a22057916w@gmail.com>
2021-10-15 10:41:44 +0800

    new sort UI example

---
+++ sortUI/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <QApplication>
+#include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QGridLayout>
+#include <QTime>
+#include <QThread>
+#include <QTextEdit>
+#include <QRadioButton>
+
+
+#endif
---
+++ sortUI/sort.cpp
@@ -0,0 +1,10 @@
+#include "main.h"
+
+#include <alogrithm>
+#include <string>
+
+using std::string;
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
