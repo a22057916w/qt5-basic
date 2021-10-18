4bc6b5b3389cce019a6aac29000d1def20aedba2
a22057916w <a22057916w@gmail.com>
2021-10-18 11:22:12 +0800

    fix syntax error

--- sortUI/sort.cpp
+++ sortUI/sort.cpp
@@ -17,8 +17,8 @@ using std::cout;
 
 /* create delay in qt */
 void delay() {
-  QTime dieTime = Qtime::currentTime().addMSecs(SLEEP_ms);
-  while(Qtime::currentTime() < dieTime)
+  QTime dieTime = QTime::currentTime().addMSecs(SLEEP_ms);
+  while(QTime::currentTime() < dieTime)
     QCoreApplication::processEvents(QEventLoop::AllEvents, 100);
 }
 /* filling array with random number */
@@ -35,7 +35,7 @@ void updateButton(QPushButton *button[], vector<int> &arr) {
 }
 
 void highlightButton(QPushButton *button) {
-  button->setStyleSheet("background-color:yellow; color->blue; font-weigth:bold;");
+  button->setStyleSheet("background-color:yellow; color->blue; font-weight:bold;");
 }
 /* set the button to normal color */
 void unhighlightButton(QPushButton *button) {
@@ -50,12 +50,12 @@ void bubblesort(vector<int> arr, QPushButton *button[]) {
   for(int i = 0; i < arr.size(); i++) {
     bool swapped = false;
     for(int j = 0; j < arr.size() - 1; j++) {
-      highlightButton(button[j])
-      highlightButton(button[j + 1])
+      highlightButton(button[j]);
+      highlightButton(button[j + 1]);
       delay();
       if(arr[j] > arr[j + 1]) {
         std::swap(arr[j], arr[j+1]);
-        setButtonVal(button[j], arr[j);
+        setButtonVal(button[j], arr[j]);
         setButtonVal(button[j+ 1], arr[j + 1]);
         delay();
         swapped = true;
@@ -66,6 +66,7 @@ void bubblesort(vector<int> arr, QPushButton *button[]) {
     // if no swapping had occoured then list is sorted
     if (!swapped)
       break;
+  }
 }
 /* run selection sort and update UI */
 void selectionsort(vector<int> arr, QPushButton *button[]) {
@@ -92,12 +93,12 @@ int main(int argc, char *argv[]) {
 
   // create layout inside window
   QGridLayout *layout = new QGridLayout(&window);
-  QPushButton *button[arr.size()];
+  QPushButton *buttons[arr.size()];
   for(int i = 0; i < arr.size(); i++) {
     string btnVal = std::to_string(arr[i]);
-    button[i] = new QPushButton(btnVal.c_str());
-    button[i]->setStyleSheet("font-weight:bold;");
-    layout->addWidget(button[i], 0, i, 1, 1);
+    buttons[i] = new QPushButton(btnVal.c_str());
+    buttons[i]->setStyleSheet("font-weight:bold;");
+    layout->addWidget(buttons[i], 0, i, 1, 1);
   }
 
   // the following two functions both work, but tr() can transfer any string to QString
@@ -122,7 +123,7 @@ int main(int argc, char *argv[]) {
     // when reset button is pressed generate another random set of array
     // and update it in GUI
     updateArray(arr);
-    updateButton(button, arr);
+    updateButton(buttons, arr);
     btnSort->setText("Sort");
   });
 
@@ -133,12 +134,12 @@ int main(int argc, char *argv[]) {
     }
     isSorting = true;
     btnReset->setVisible(false);
-    btnSort->setText("Sorting...")
+    btnSort->setText("Sorting...");
     // if bubble sort radio button is clicked do binary sort
     // else do selection sort
     radioBubble->isChecked() ? bubblesort(arr, buttons) : selectionsort(arr, buttons);
     btnReset->setVisible(true);
-    btnSort->setText("Sort")
+    btnSort->setText("Sort");
     isSorting = false;
   });
 
